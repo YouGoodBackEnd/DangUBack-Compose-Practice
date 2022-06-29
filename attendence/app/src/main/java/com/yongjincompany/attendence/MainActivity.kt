0116c55d4af903759b626669364e779ea206bcaa
Leesin0222 <dragon02229@naver.com>
2022-06-29 20:31:25 +0900

    ⚡:: Naviation 설정

--- attendence/app/src/main/java/com/yongjincompany/attendence/MainActivity.kt
+++ attendence/app/src/main/java/com/yongjincompany/attendence/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.yongjincompany.attendence
 
+import RegisterScreen
+import android.graphics.Region
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.yongjincompany.attendence.composables.BackGroundImage
+import com.yongjincompany.attendence.composables.LoginArea
 import com.yongjincompany.attendence.composables.LoginScreen
+import com.yongjincompany.attendence.ui.Screen
 import com.yongjincompany.attendence.ui.theme.AttendenceTheme
 import com.yongjincompany.attendence.ui.theme.PrimaryColor
 
@@ -15,12 +26,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AttendenceTheme {
-                androidx.compose.material.Surface(color = PrimaryColor,
+                Surface(color = PrimaryColor,
                     modifier = Modifier.fillMaxSize()) {
-                    LoginScreen()
+                    Navigation()
                 }
             }
         }
     }
 }
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController as NavHostController, startDestination = Screen.RegisterScreen.route) {
+        composable(route = Screen.LoginScreen.route) {
+            LoginScreen(navController = navController)
+        }
+        composable(route = Screen.RegisterScreen.route) {
+            RegisterScreen(navController = navController)
+        }
+    }
+}
+
+
 
